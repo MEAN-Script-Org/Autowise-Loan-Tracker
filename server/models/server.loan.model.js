5adf1a2e59d9b94e3da1b4738386e457e557a518
Michael Ngo <michaeltngo11@gmail.com>
2017-10-13 20:25:50 -0400

    update

--- server/models/server.loan.model.js
+++ server/models/server.loan.model.js
@@ -3,7 +3,39 @@
 var mongoose = require('mongoose') ;
 
 // Define loan schema
+/* 
+Status : enum?/strings. 
+	MUST BE VISIBLE on description/without clicking on them
+	They can be
+	RECEIVED/SUBMITTED
+		RECEIVED => FROM OFFICE
+		SUBMITTED => FROM BANK
+	PENDING => everything in the process
+	VERIFIED => REVIEWED APP, things are ok
+	APPROVED/DENIED
+	Archived : bool. True if (APPROVED/DENIED)
+Costs
+	Taxes
+	Warranties ? => CHECK PICTURES. NEED MORE DETAILS
+Types
+	Auto Loan
+	Repair
+	Admin - need more details on this. Administrative fees?
+Trades
+	default to false/'[]' (empty array - which is 'falsey')
+	ADMIN puts in trade information later
+Comments
+	Content : array with messages in reverse chronological order (easy to flip in angular)
+		Need more clarification of what to put 'on top' as important message
+	Date/timestamps - format tba later. not that important
+	visibleToConsumer : bool
+	important : bool
+	CHANGE TO MOCKUP: Same area as normal info
+
+
+*/
 var loanSchema = new mongoose.Schema({
+	comments: String,
 	
 });
 
---
+++ server/models/user.loan.model.js
@@ -0,0 +1,51 @@
+
+// Dependencies
+var mongoose = require('mongoose') ;
+
+// Define user schema
+var userSchema = new mongoose.Schema({
+  username: String,
+  password: hash,
+  isAdmin: bool,
+
+  created_at: Date,
+  updated_at: Date
+
+  
+});
+
+// Any pre-processing on saving a loan document?
+userSchema.pre('save', function(next) {
+	
+ if (!this.username) {
+	var err = new Error('something went wrong');
+ 	next(err);
+	}
+
+ if (!this.password) {
+ 	var err = new Error('something went wrong');
+ 	next(err);
+ //callback error
+ 	}
+
+
+	var currentDate = new Date();
+
+	// if created_at doesn't exist, add to that field
+  	this.updated_at = currentDate;
+
+  	if (!this.created_at)
+    	this.created_at = currentDate;
+
+
+	next() ;
+});
+
+// Create user model from schema
+var User = mongoose.model('User', userSchema) ;
+
+// Export user model to application
+module.exports = User ;
+
+//created on, created by, modified on, modified by (who created users)
+
