295d0fdeeb8a2d5eac788c9f093cece5c3e5378f
Maximus32 <dunevitz32@gmail.com>
2017-10-20 16:38:07 -0400

    Changing unit test code

--- unit_testing/server/test.loan.database.js
+++ unit_testing/server/test.loan.database.js
@@ -5,8 +5,9 @@ var should   = require('should') ;
 var mongoose = require('mongoose') ;
 
 // Dependencies - local files
-var config_db = require('../../server/server.db_config.js') ;
+//var config_db = require('../../server/server.db_config.js') ;
 var Loan      = require('../../server/db/loans.model.js') ;
+var express   = require('../../server/express.js') ;
 
 //======================================================================================================================
 // TEST GROUP I - BACK-END DATABASE CRUD FUNCTIONALITY
@@ -15,7 +16,7 @@ describe('TEST GROUP I - BACK-END DATABASE CRUD FUNCTIONALITY', function () {
   
   // Specify database connection
   before(function(done) {
-    mongoose.connect(config_db.db.uri) ;
+    express.init() ;
     
     done() ;
   }) ;
@@ -26,10 +27,10 @@ describe('TEST GROUP I - BACK-END DATABASE CRUD FUNCTIONALITY', function () {
   // Database loan object
   var test_db_loan ;
   
-  // Testing loan object that is well-defined
+  // Testing loan objects that are well-defined
   var test_loan_ok = new Loan({
     status: '',
-    types: 'Car Loan',
+    types: 'Auto Loan',
     costs: { taxes: 3000.00, warranties: 200.00 },
     trades: {},
     comments: ['This is a test']
@@ -39,7 +40,7 @@ describe('TEST GROUP I - BACK-END DATABASE CRUD FUNCTIONALITY', function () {
   var test_loan_bad = new Loan({});
   
   //--------------------------------------------------------------------------------------------------------------------
-  // Test #1.0: Loan is created and saved succesfully when missing status field
+  // Test #1.0: Loan is created and saved succesfully when missing 'status' field
   //--------------------------------------------------------------------------------------------------------------------
   it('Test #1.0: Loan is created and saved succesfully when missing \'status\' field', function(done) {
     test_loan_ok.save(function (err) {
@@ -62,18 +63,40 @@ describe('TEST GROUP I - BACK-END DATABASE CRUD FUNCTIONALITY', function () {
   });
   
   //--------------------------------------------------------------------------------------------------------------------
-  // Test #1.2: Loan 'status' field updated to 'RECEIVED' due to unspecified 'status'
+  // Test #1.2: Loan is created and saved succesfully when missing 'types' field
   //--------------------------------------------------------------------------------------------------------------------
-  it('Test #1.2: Loan \'status\' field updated to \'RECEIVED\' due to unspecified \'status\'', function(done) {
+  it('Test #1.2: Loan is created and saved succesfully when missing \'types\' field', function(done) {
+    test_db_loan.types = '' ;
+    
+    test_db_loan.save(function (err) {
+      should.not.exist(err) ;
+      
+      done() ;
+    });
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #1.3.0: Loan 'status' field updated to 'RECEIVED' due to unspecified 'status'
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #1.3.0: Loan \'status\' field updated to \'RECEIVED\' due to unspecified \'status\'', function(done) {
     test_db_loan.status.should.equal('RECEIVED') ;
     
     done() ;
   });
   
   //--------------------------------------------------------------------------------------------------------------------
-  // Test #1.3: Other Loan fields match those of uploaded Loan
+  // Test #1.3.1: Loan 'types' field updated to 'Auto Loan' due to unspecified 'types'
   //--------------------------------------------------------------------------------------------------------------------
-  it('Test #1.3: Other Loan fields match those of uploaded Loan', function(done) {
+  it('Test #1.3.1: Loan \'types\' field updated to \'Auto Loan\' due to unspecified \'types\'', function(done) {
+    test_db_loan.types.should.equal('Auto Loan') ;
+    
+    done() ;
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #1.4: Other Loan fields match those of uploaded Loan
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #1.4: Other Loan fields match those of uploaded Loan', function(done) {
     test_db_loan.types.should.equal(test_loan_ok.types) ;
     test_db_loan.costs.taxes.should.equal(test_loan_ok.costs.taxes) ;
     test_db_loan.costs.warranties.should.equal(test_loan_ok.costs.warranties) ;
@@ -85,9 +108,9 @@ describe('TEST GROUP I - BACK-END DATABASE CRUD FUNCTIONALITY', function () {
   });
   
   //--------------------------------------------------------------------------------------------------------------------
-  // Test #1.4: Error is thrown when attempting to save without 'types' 
+  // Test #1.5: Error is thrown when attempting to save without 'types' field
   //--------------------------------------------------------------------------------------------------------------------
-  it('Test #1.4: Error is thrown when attempting to save without \'types\' field', function(done) {
+  it('Test #1.5: Error is thrown when attempting to save without \'types\' field', function(done) {
     test_loan_bad.save(function (err) {
       should.exist(err) ;
       
@@ -96,9 +119,9 @@ describe('TEST GROUP I - BACK-END DATABASE CRUD FUNCTIONALITY', function () {
   });
   
   //--------------------------------------------------------------------------------------------------------------------
-  // Test #1.5: Loan can be deleted from the database
+  // Test #1.6: Loan can be deleted from the database
   //--------------------------------------------------------------------------------------------------------------------
-  it('Test #1.5: Loan can be deleted from the database', function(done) {
+  it('Test #1.6: Loan can be deleted from the database', function(done) {
     test_db_loan.remove(test_db_loan, function (err) {
       should.not.exist(err) ;
       
@@ -107,9 +130,9 @@ describe('TEST GROUP I - BACK-END DATABASE CRUD FUNCTIONALITY', function () {
   });
   
   //--------------------------------------------------------------------------------------------------------------------
-  // Test #1.6: Loan is successfully removed from the database
+  // Test #1.7: Loan is successfully removed from the database
   //--------------------------------------------------------------------------------------------------------------------
-  it('Test #1.6: Loan is successfully removed from the database', function(done) {
+  it('Test #1.7: Loan is successfully removed from the database', function(done) {
     Loan.find({_id: test_db_loan._id}, function(err, loans) {
       should.not.exist(err) ;
       should.not.exist(loans[0]) ;
