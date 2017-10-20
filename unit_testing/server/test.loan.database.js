119bfe1798ee8aae08d6f4723572e6ff218ef7d8
Maximus32 <dunevitz32@gmail.com>
2017-10-20 01:11:20 -0400

    Working on routes testing via HTTP requests. Also made some significant changes to the CRUD file for loans

--- unit_testing/server/test.loan.database.js
+++ unit_testing/server/test.loan.database.js
@@ -1,14 +1,17 @@
 'use strict' ;
 
-// Dependencies
-var should = require('should') ;
+// Dependencies - installed modules
+var should   = require('should') ;
 var mongoose = require('mongoose') ;
 
+// Dependencies - local files
 var config_db = require('../../server/server.db_config.js') ;
-var Loan = require('../../server/db/loans.model.js') ;
+var Loan      = require('../../server/db/loans.model.js') ;
 
-// TEST I: BACK-END DATABASE CRUD FUNCTIONALITY
-describe('Loan backend unit test - Saving functionality:', function () {
+//======================================================================================================================
+// TEST GROUP I - BACK-END DATABASE CRUD FUNCTIONALITY
+//======================================================================================================================
+describe('TEST GROUP I - BACK-END DATABASE CRUD FUNCTIONALITY', function () {
   
   // Specify database connection
   before(function(done) {
@@ -17,90 +20,101 @@ describe('Loan backend unit test - Saving functionality:', function () {
     done() ;
   }) ;
   
-  describe('Creating, saving, modifying, and deleting Loan objects from the database', function () {
-    
-    // Set timeout to 5 seconds
-    this.timeout(5000) ;
-    
-    // Database loan object
-    var test_db_loan ;
-    
-    // Testing loan object that is well-defined
-    var test_loan_ok = new Loan({
-      status: '',
-      types: 'Car Loan',
-      costs: { taxes: 3000.00, warranties: 200.00 },
-      trades: {},
-      comments: ['This is a test']
-    });
-    
-    // Testing loan object that is poorly defined
-    var test_loan_bad = new Loan({});
-    
-    // Test #1.0: Loan is created and saved succesfully when missing status field
-    it('Test #1.0: Loan is created and saved succesfully when missing \'status\' field', function(done) {
-      test_loan_ok.save(function (err) {
-        should.not.exist(err) ;
-        
-        done() ;
-      });
-    });
-    
-    // Test #1.1: Loan is created AND uploaded successfully (can be fetched from the database)
-    it('Test #1.1: Loan is created AND uploaded successfully (can be fetched from the database)', function(done) {
-      Loan.find({comments: test_loan_ok.comments}, function(err, loans) {
-        should.not.exist(err) ;
-        test_db_loan = loans[0] ;
-        
-        done() ;
-      });
-    });
-    
-    // Test #1.2: Loan 'status' field updated to 'RECEIVED' due to unspecified 'status'
-    it('Test #1.2: Loan \'status\' field updated to \'RECEIVED\' due to unspecified \'status\'', function(done) {
-      test_db_loan.status.should.equal('RECEIVED') ;
+  // Set timeout to 5 seconds
+  this.timeout(5000) ;
+  
+  // Database loan object
+  var test_db_loan ;
+  
+  // Testing loan object that is well-defined
+  var test_loan_ok = new Loan({
+    status: '',
+    types: 'Car Loan',
+    costs: { taxes: 3000.00, warranties: 200.00 },
+    trades: {},
+    comments: ['This is a test']
+  });
+  
+  // Testing loan object that is poorly defined
+  var test_loan_bad = new Loan({});
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #1.0: Loan is created and saved succesfully when missing status field
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #1.0: Loan is created and saved succesfully when missing \'status\' field', function(done) {
+    test_loan_ok.save(function (err) {
+      should.not.exist(err) ;
       
       done() ;
     });
-    
-    // Test #1.3: Other Loan fields match those of uploaded Loan
-    it('Test #1.3: Other Loan fields match those of uploaded Loan', function(done) {
-      test_db_loan.types.should.equal(test_loan_ok.types) ;
-      test_db_loan.costs.taxes.should.equal(test_loan_ok.costs.taxes) ;
-      test_db_loan.costs.warranties.should.equal(test_loan_ok.costs.warranties) ;
-      test_db_loan.trades.should.equal(test_loan_ok.trades) ;
-      test_db_loan.comments[0].should.equal(test_loan_ok.comments[0]) ;
-      should.not.exist(test_db_loan.comments[1])
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #1.1: Loan is created AND uploaded successfully (can be fetched from the database)
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #1.1: Loan is created AND uploaded successfully (can be fetched from the database)', function(done) {
+    Loan.find({comments: test_loan_ok.comments}, function(err, loans) {
+      should.not.exist(err) ;
+      test_db_loan = loans[0] ;
       
       done() ;
     });
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #1.2: Loan 'status' field updated to 'RECEIVED' due to unspecified 'status'
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #1.2: Loan \'status\' field updated to \'RECEIVED\' due to unspecified \'status\'', function(done) {
+    test_db_loan.status.should.equal('RECEIVED') ;
     
-    // Test #1.4: Error is thrown when attempting to save without 'types' field
-    it('Test #1.4: Error is thrown when attempting to save without \'types\' field', function(done) {
-      test_loan_bad.save(function (err) {
-        should.exist(err) ;
-        
-        done() ;
-      });
+    done() ;
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #1.3: Other Loan fields match those of uploaded Loan
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #1.3: Other Loan fields match those of uploaded Loan', function(done) {
+    test_db_loan.types.should.equal(test_loan_ok.types) ;
+    test_db_loan.costs.taxes.should.equal(test_loan_ok.costs.taxes) ;
+    test_db_loan.costs.warranties.should.equal(test_loan_ok.costs.warranties) ;
+    test_db_loan.trades.should.equal(test_loan_ok.trades) ;
+    test_db_loan.comments[0].should.equal(test_loan_ok.comments[0]) ;
+    should.not.exist(test_db_loan.comments[1])
+    
+    done() ;
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #1.4: Error is thrown when attempting to save without 'types' 
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #1.4: Error is thrown when attempting to save without \'types\' field', function(done) {
+    test_loan_bad.save(function (err) {
+      should.exist(err) ;
+      
+      done() ;
     });
-    
-    // Test #1.5: Loan can be deleted from the database
-    it('Test #1.5: Loan can be deleted from the database', function(done) {
-      test_db_loan.remove(test_db_loan, function (err) {
-        should.not.exist(err) ;
-        
-        done() ;
-      });
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #1.5: Loan can be deleted from the database
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #1.5: Loan can be deleted from the database', function(done) {
+    test_db_loan.remove(test_db_loan, function (err) {
+      should.not.exist(err) ;
+      
+      done() ;
     });
-    
-    // Test #1.6: Loan is successfully removed from the database
-    it('Test #1.6: Loan is successfully removed from the database', function(done) {
-      Loan.find({_id: test_db_loan._id}, function(err, loans) {
-        should.not.exist(err) ;
-        should.not.exist(loans[0]) ;
-        
-        done() ;
-      });
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #1.6: Loan is successfully removed from the database
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #1.6: Loan is successfully removed from the database', function(done) {
+    Loan.find({_id: test_db_loan._id}, function(err, loans) {
+      should.not.exist(err) ;
+      should.not.exist(loans[0]) ;
+      
+      done() ;
     });
   });
 });
---
+++ unit_testing/server/test.loan.routes.js
@@ -0,0 +1,136 @@
+'use strict' ;
+
+// Dependencies - installed modules
+var should   = require('should') ;
+var request  = require('supertest') ;
+
+// Dependencies - local files
+var Loan      = require('../../server/db/loans.model.js') ;
+var express   = require('../../server/express.js') ;
+
+//======================================================================================================================
+// TEST GROUP II - FRONT-END LOAN HTTP ROUTING
+//======================================================================================================================
+describe('TEST GROUP II - FRONT-END LOAN HTTP ROUTING', function () {
+  
+  // Specify HTTP agent
+  var agent ;
+  before(function(done) {
+    agent = request.agent(express.init()) ;
+    
+    done() ;
+  });
+  
+  // Set timeout to 5 seconds
+  this.timeout(5000) ;
+  
+  // Database loan object ID
+  var test_loan_id ;
+  
+  // Testing loan object that is well-defined
+  var test_loan_ok = new Loan({
+    status: '',
+    types: 'Car Loan',
+    costs: { taxes: 3000.00, warranties: 200.00 },
+    trades: {},
+    comments: ['This is a test']
+  });
+  
+  // Testing loan object that is poorly defined
+  var test_loan_bad = new Loan({});
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #2.0: Loan is created and saved succesfully -> HTTP response body is JSON of posted Loan
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #2.0: Loan is created and saved succesfully -> HTTP response body is JSON of posted Loan', function(done) {
+    agent.post('/api/loans/').send(test_loan_ok).expect(200).end(function(err, res) {
+      should.not.exist(err) ;
+      should.exist(res) ;
+      should.exist(res.body._id) ;
+      
+      test_loan_id = res.body._id ;
+      
+      done() ;
+    });
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #2.1: Loan is created AND uploaded successfully -> HTTP response body is JSON of posted Loan
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #2.1: Loan is created AND uploaded successfully -> HTTP response body is JSON of posted Loan', function(done) {
+    agent.get('/api/loan/' + test_loan_id).expect(200).end(function(err, res) {
+      should.exist(res) ;
+      
+      res.body.status.should.equal('RECEIVED') ;
+      res.body.types.should.equal(test_loan_ok.types) ;
+      res.body.costs.taxes.should.equal(test_loan_ok.costs.taxes) ;
+      res.body.costs.warranties.should.equal(test_loan_ok.costs.warranties) ;
+      res.body.trades.should.equal(test_loan_ok.trades) ;
+      res.body.comments[0].should.equal(test_loan_ok.comments[0]) ;
+      should.not.exist(res.body.comments[1]) ;
+      
+      done() ;
+    });
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #2.2: All Loans may be retrieved -> HTTP response body is JSON array of Loans
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #2.2: All Loans may be retrieved -> HTTP response body is JSON array of Loans', function(done) {
+    agent.get('/api/listings').expect(200).end(function(err, res) {
+      should.not.exist(err) ;
+      should.exist(res) ;
+      
+      res.body.should.have.length(1) ; // Should be a single Loan in the database
+      
+      done() ;
+    });
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #2.3: Error is thrown when attempting to update a loan to an unspecified 'status'
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #2.3: Error is thrown when attempting to update a loan to an unspecified \'status\'', function(done) {
+    agent.put('/api/loan/' + test_loan_id).send(test_loan_bad).expect(400).end(function(err, res) {
+      should.exist(err) ;
+      
+      done() ;
+    });
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #2.4: May update a loan successfully -> HTTP response body is JSON of posted Loan
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #2.4: May update a loan successfully -> HTTP response body is JSON of posted Loan', function(done) {
+    test_loan_ok.status = 'PENDING' ;
+    
+    agent.put('/api/loan/' + test_loan_id).send(test_loan_ok).expect(200).end(function(err, res) {
+      should.not.exist(err) ;
+      
+      res.body.status.should.equal(test_loan_ok.status) ;
+      
+      done() ;
+    });
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #2.5: May delete a Loan -> HTTP response body is JSON deleted Loan
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #2.5: May delete a Loan -> HTTP response body is JSON deleted Loan', function(done) {
+    agent.delete('/api/loan/' + test_loan_id).expect(200).end(function(err, res) {
+      should.not.exist(err) ;
+      should.exist(res) ;
+      
+      done() ;
+    });
+  });
+  
+  //--------------------------------------------------------------------------------------------------------------------
+  // Test #2.6: Loan is successfully removed from database
+  //--------------------------------------------------------------------------------------------------------------------
+  it('Test #2.6: Loan is successfully removed from database', function(done) {
+    agent.get('/api/loan/' + test_loan_id).expect(400).end(function(err, res) {
+      done() ;
+    });
+  });
+});
