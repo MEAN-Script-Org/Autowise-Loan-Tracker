f4095afe543b4d12bf918d37892576ccc5fbe52a
hschorr3 <hschorr3@ufl.edu>
2017-11-08 15:24:08 -0500

    Committing so I can switch branches

---
+++ client/angular/userPerm.controller.js
@@ -0,0 +1,8 @@
+$scope.fakeUsers = [
+	{username: "Harrison", dob: "8/20/97"},
+	{username: "Marcial", dob: "5/12/96"},
+	{username: "Steven", dob: "4/3/96"},
+	{username: "Max", dob: "7/1/96"},
+	{username: "Michael", dob: "2/24/97"},
+	{username: "lalala", dob: "6/4/96"},
+]
