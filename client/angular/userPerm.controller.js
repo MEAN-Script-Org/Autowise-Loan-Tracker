3504c5fcc2629a652cba6c48a29179b8f839ccb5
Marcial <marcial.abrahantes@gmail.com>
2017-11-28 00:59:59 -0500

    userPerm design

--- client/angular/userPerm.controller.js
+++ client/angular/userPerm.controller.js
@@ -1,80 +1,77 @@
- angular.module('SWEApp').controller('userPerm', ['$rootScope', '$scope', '$location', 'Factory',
-     function($rootScope, $scope, $location, Factory) {
+angular.module('SWEApp').controller('userPerm', ['$rootScope', '$scope', '$location', 'Factory',
+  function($rootScope, $scope, $location, Factory) {
 
-         $rootScope.fake_users = {};
+    $rootScope.users = {};
 
-         Factory.getAllUsers().then(
-             function(res) {
-                 //  buttons will modify 'isAdmin'
-                 $rootScope.fake_users = res.data;
-                 console.log($rootScope.fake_users);
-             },
-             function(err) {
-                 alert(err);
-             });
+    Factory.getAllUsers().then(
+      function(res) {
+        //  buttons will modify 'isAdmin'
+        $rootScope.users = res.data;
+        $rootScope.users.forEach(function(item, index, obj) {
+          obj[index].dob = new Date(item.dob).toLocaleDateString('en-IR');
+        })
+      },
+      function(err) {
+        alert(err);
+    });
 
-         $scope.init = function() {
-             $scope.query = "";
-             $scope.visible = "visible";
-         }
+    $scope.init = function() {
+      $scope.query = [];
+      $scope.visible = "visible";
+    }
+    
+    // TODO: copy 'prepare' thing
+    // only have a delete frontend thing
+    // have a color coding for 'isAdmin'
 
-         // BACKUP THESE TWO FILES, CHECKOUT 'merged-sprint-3'
-         // then 'git checkout -b [new_branch_name_here]'
-         // then actually implement stuff
-         // Flow:
-         // view => controller => Factory => user models
-         // view takes an id from ng-repeat, controller is an async call
-         // backend should be implemented, just add .delete in ("/users") in 
-         // routes.js
-
-         $scope.removeUser = function(userID) {
-             Factory.deleteUser(userID).then(
-                 function(response) {
-                     $rootScope.fake_users.some(function(item, index, fake_users) {
-                         if (item._id) {
-                             if (item._id == userID) {
-                                 fake_users.splice(index, 1);
-                                 return true;
-                             }
-                         }
-                     });
-                 },
-                 function(err) {
-                     alert("There was a problem deleting this User.");
-                     console.log(err);
-                 }
-             );
-         }
+    // $scope.removeUser = function(userID) {
+    //   Factory.deleteUser(userID).then(
+    //     function(response) {
+    //       $rootScope.users.some(function(item, index, users) {
+    //         if (item._id) {
+    //           if (item._id == userID) {
+    //             users.splice(index, 1);
+    //             return true;
+    //           }
+    //         }
+    //       });
+    //     },
+    //     function(err) {
+    //       alert("There was a problem deleting this User.");
+    //       console.log(err);
+    //     }
+    //   );
+    // }
 
 
-         $scope.makeAdmin = function(userID, isAdmin) {
-             Factory.makeAdmin(userID).then(
-                 function(response) {
-                     $rootScope.fake_users.some(function(item, fake_users) {
-                         if (item._id == userID) {
-                             fake_users.isAdmin == true;
-                         }
-                     });
-                 },
-                 function(err) {
-                     alert("Error making user an admin.");
-                     console.log(err);
-                 });
-         }
+    // $scope.makeAdmin = function(userID, isAdmin) {
+    //   Factory.makeAdmin(userID).then(
+    //     function(response) {
+    //       $rootScope.users.some(function(item, users) {
+    //         if (item._id == userID) {
+    //           users.isAdmin == true;
+    //         }
+    //       });
+    //     },
+    //     function(err) {
+    //       alert("Error making user an admin.");
+    //       console.log(err);
+    //     });
+    // }
 
-         $scope.makeUser = function(userID, isAdmin) {
-             Factory.makeUser(userID).then(
-                 function(response) {
-                     $rootScope.fake_users.some(function(item, fake_users) {
-                         if (item._id == userID) {
-                             fake_users.isAdmin = false;
-                         }
-                     });
-                 },
-                 function(err) {
-                     alert("Error updating to user.");
-                     console.log(err);
-                 });
-         }
-     }
- ]);
+    // $scope.makeUser = function(userID, isAdmin) {
+    //   Factory.makeUser(userID).then(
+    //     function(response) {
+    //       $rootScope.users.some(function(item, users) {
+    //         if (item._id == userID) {
+    //           users.isAdmin = false;
+    //         }
+    //       });
+    //     },
+    //     function(err) {
+    //       alert("Error updating to user.");
+    //       console.log(err);
+    //     });
+    // }
+  }
+]);
