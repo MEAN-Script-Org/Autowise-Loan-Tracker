8621360e0d4e386c854185d0019bf52dae993ed0
Michael Ngo <michaeltngo11@gmail.com>
2017-12-02 14:36:10 -0500

    MN fixing account password change

--- client/angular/account.controller.js
+++ client/angular/account.controller.js
@@ -13,5 +13,22 @@ angular.module('SWEApp').controller('UserAccounts', ['$rootScope', '$scope', '$l
           $rootScope.user = res.data; console.log(res.data) ;
         });
       }
+
+      $scope.changePasswordSubmit = function(pwd, pw1, pw2) {
+
+        if (pw1 == pw2) {
+          alert("New password is: '" + pw1 + "'");
+        }
+
+        else {
+          alert("Passwords do not match!")
+        } 
+      }
+
+      $scope.resetForm = function() {
+     //$scope.resetme.reset();
+        document.getElementById('resetme').reset();
+      };
+
     }
 ]);
