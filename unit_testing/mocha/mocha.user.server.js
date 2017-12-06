0e4d65b286b1580f95837c96bf30b5fc0416272e
Maximus32 <dunevitz32@gmail.com>
2017-12-06 10:00:05 -0500

    Removed 'status' test for users and renumbered tests

--- unit_testing/mocha/mocha.user.server.js
+++ unit_testing/mocha/mocha.user.server.js
@@ -72,19 +72,10 @@ describe('TEST GROUP III - USER BACK-END DATABASE CRUD FUNCTIONALITY', function 
     });
   });
   
-  // //--------------------------------------------------------------------------------------------------------------------
-  // // Test #3.2: User 'status' field updated to 'RECEIVED' due to unspecified 'status'
-  // //--------------------------------------------------------------------------------------------------------------------
-  // it('Test #3.2: User \'status\' field updated to \'RECEIVED\' due to unspecified \'status\'', function(done) {
-  //   test_db_User.status.should.equal('RECEIVED') ;
-    
-  //   done() ;
-  // });
-  
   //--------------------------------------------------------------------------------------------------------------------
-  // Test #3.3: Other User fields match those of uploaded User
+  // Test #3.2: Other User fields match those of uploaded User
   //--------------------------------------------------------------------------------------------------------------------
-  it('Test #3.3: Other User fields match those of uploaded User', function(done) {
+  it('Test #3.2: Other User fields match those of uploaded User', function(done) {
     
 
     // Check a few select fields
@@ -95,9 +86,9 @@ describe('TEST GROUP III - USER BACK-END DATABASE CRUD FUNCTIONALITY', function 
   });
   
   //--------------------------------------------------------------------------------------------------------------------
-  // Test #3.4: User can be deleted from the database
+  // Test #3.3: User can be deleted from the database
   //--------------------------------------------------------------------------------------------------------------------
-  it('Test #3.4: User can be deleted from the database', function(done) {
+  it('Test #3.3: User can be deleted from the database', function(done) {
     test_db_user.remove(test_db_user, function (err) {
       should.not.exist(err) ;
       
@@ -106,9 +97,9 @@ describe('TEST GROUP III - USER BACK-END DATABASE CRUD FUNCTIONALITY', function 
   });
   
   //--------------------------------------------------------------------------------------------------------------------
-  // Test #3.5: User is successfully removed from the database
+  // Test #3.4: User is successfully removed from the database
   //--------------------------------------------------------------------------------------------------------------------
-  it('Test #3.5: User is successfully removed from the database', function(done) {
+  it('Test #3.4: User is successfully removed from the database', function(done) {
     User.find({_id: test_db_user._id}, function(err, users) {
       should.not.exist(err) ;
       should.not.exist(users[0]) ;
@@ -118,9 +109,9 @@ describe('TEST GROUP III - USER BACK-END DATABASE CRUD FUNCTIONALITY', function 
   });
   
   //--------------------------------------------------------------------------------------------------------------------
-  // Test #3.6: Poorly-defined User cannot be saved in the database
+  // Test #3.5: Poorly-defined User cannot be saved in the database
   //--------------------------------------------------------------------------------------------------------------------
-  it('Test #3.6: Poorly-defined User cannot be saved in the database', function(done) {
+  it('Test #3.5: Poorly-defined User cannot be saved in the database', function(done) {
     test_user_bad.save(function (err) {
       should.exist(err) ;
       
